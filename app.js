45d611b20369af0551a71f3ab7f5aea474312f1d
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2021-09-27 13:59:32 +0530

    fetch wire call

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var btnTranslate= document.querySelector("#btn-translator");
 var txtInput= document.querySelector("#txt-input");
 var txtOutput=document.querySelector("#output");
+var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=";
+function getTranslatorUrl(text){
+    return serverUrl+text
+}
 
 
 
 function clickEventHandler(){
+ var inputText = txtInput.value;
+ var url= getTranslatorUrl(inputText);
  
-    txtOutput.innerText= "jjsjs "+ txtInput.value;
+fetch(url)
+.then(response=> response.json())
+.then(json=>console.log(json.contents.translated))
+
+ 
+ 
 
    
 }
